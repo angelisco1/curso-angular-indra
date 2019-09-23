94a3d12453e615cb9ac08afcd47812a3458b3e64
angelvil92 <angelvil92@gmail.com>
2019-09-23 20:14:49 +0200

    Aadido sonido y ejemplos de comunicacion entre componentes

--- proyecto/src/app/cmp-comunicacion-componentes/cmp-a/cmp-a.component.ts
+++ proyecto/src/app/cmp-comunicacion-componentes/cmp-a/cmp-a.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cmp-a',
@@ -6,12 +6,16 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./cmp-a.component.css']
 })
 export class CmpAComponent implements OnInit {
-
+  @Output() nombreCambiado = new EventEmitter<string>();
   @Input() miNombre = 'Sansa';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  avisarCambio(nuevoNombre) {
+    this.nombreCambiado.emit(nuevoNombre)
   }
 
 }
--- proyecto/src/app/cmp-comunicacion-componentes/cmp-b/cmp-b.component.ts
+++ proyecto/src/app/cmp-comunicacion-componentes/cmp-b/cmp-b.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cmp-b',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cmp-b.component.css']
 })
 export class CmpBComponent implements OnInit {
-
+  @Input() miNombre = ''
+  @Input() nombreHermano = ''
   constructor() { }
 
   ngOnInit() {
